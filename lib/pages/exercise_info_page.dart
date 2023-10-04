255eda4f7b11570b0ab46d84a5bb223b2bfbfcd8
Ilya_Kiselev <mybutifullie@gmail.com>
2023-10-04 16:02:20 +1000

    исправил отображение страници упражнения

--- lib/pages/exercise_info_page.dart
+++ lib/pages/exercise_info_page.dart
@@ -2,10 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/classes/exercise.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class ExerciseInfoPage extends StatelessWidget {
+class ExerciseInfoPage extends StatefulWidget {
   const ExerciseInfoPage({super.key, required this.exercise});
 
   final Exercise exercise;
+
+  @override
+  State<ExerciseInfoPage> createState() => _ExerciseInfoPageState();
+}
+
+class _ExerciseInfoPageState extends State<ExerciseInfoPage> {
+  late YoutubePlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = YoutubePlayerController(
+      initialVideoId: widget.exercise.videoId,
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute: true,
+        loop: true,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,55 +41,47 @@ class ExerciseInfoPage extends StatelessWidget {
       color: theme.colorScheme.onPrimary,
     );
 
-    YoutubePlayerController controller = YoutubePlayerController(
-      initialVideoId: exercise.videoId,
-      flags: YoutubePlayerFlags(
-        autoPlay: false,
-        mute: true,
-        loop: true,
-      ),
-    );
-
     return Scaffold(
         appBar: AppBar(
-          title: Text(exercise.name),
+          title: Text(widget.exercise.name),
         ),
         body: Container(
           constraints: BoxConstraints.expand(),
           color: Theme.of(context).colorScheme.primaryContainer,
-          child: Column(mainAxisSize: MainAxisSize.max, children: [
+          child: ListView(children: [
             Card(
               color: theme.colorScheme.primary,
               shadowColor: theme.shadowColor,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  exercise.name,
+                  widget.exercise.name,
                   style: headerStyle,
                 ),
               ),
             ),
-            // описание
-            Expanded(
-              child: Text('как правиьно делать упражнение'),
-            ),
+
             Text('Иллюстрация:'),
             // image
-            if (exercise.imageUrl.isNotEmpty)
+            if (widget.exercise.imageUrl.isNotEmpty)
               Image.asset(
-                exercise.imageUrl,
+                widget.exercise.imageUrl,
                 fit: BoxFit.contain,
                 alignment: Alignment.center,
               ),
             Text('Видео-пример на YouTube:'),
-            if (exercise.videoId.isNotEmpty)
+            if (widget.exercise.videoId.isNotEmpty)
               // video
               YoutubePlayer(
                 controller: controller,
                 showVideoProgressIndicator: true,
                 progressIndicatorColor:
                     Theme.of(context).colorScheme.primaryContainer,
-              )
+              ),
+            // описание
+            Expanded(
+              child: Text(widget.exercise.info),
+            ),
           ]),
         ));
   }
